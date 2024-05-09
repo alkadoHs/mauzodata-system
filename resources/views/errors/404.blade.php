30b1d8260f9589840347920a344cc612695389ab
alkadoHs <alkadosichone@gmail.com>
2024-05-09 04:12:40 +0300

    Error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Page Not Found</title>
+    <style>
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100dvh;
+        }
+    </style>
+</head>
+<body>
+    <h2>{{ __('404 | Page not found.') }}</h2>
+</body>
+</html>
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sever Error</title>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+            align-items: center;
+            justify-content: center;
+            height: 100dvh;
+            padding: 15px;
+        }
+    </style>
+</head>
+<body>
+    <h2>{{ __('500') }}</h2>
+    <p>Sorry, it looks like something goes wrong! Mauzodata team has notified and will solve it soon. You can refresh this page to proceed with other activities.</p>
+</body>
+</html>
